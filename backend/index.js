9a431d91bfd92a7106b26d0e4b1bcd14a53099df
hoang <vinhyenvodoi98@gmail.com>
2020-03-24 15:57:25 +0700

    init backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const subscriptionHandler = require('./subscriptionHandler');
+
+var port = process.env.PORT || 30001;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '*', // allow to server to accept request from different origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true // allow session cookie from browser to pass through
+  })
+);
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/subscription', subscriptionHandler.handlePushNotificationSubscription);
+app.get('/subscription/:id', subscriptionHandler.sendPushNotification);
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
